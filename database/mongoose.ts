1b146a6936825ff5cf0cab71a0fa27711704b123
PetrescuRobert <petrescu.robert69@gmail.com>
2025-10-06 19:19:26 +0300

    database setup

---
+++ database/mongoose.ts
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const MONGO_URI = process.env.MONGO_URI;
+
+declare global {
+    var mongooseCache: {
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+    };
+}
+
+let cache = global.mongooseCache;
+
+if (!cache) {
+    cache = global.mongooseCache = {
+        conn: null,
+        promise: null,
+    };
+}
+
+const connectToDatabase = async () => {
+    if (!MONGO_URI) {
+        throw new Error('MONGO_URI is not defined in the .env file');
+    }
+
+    if (cache.conn) {
+        return cache.conn;
+    }
+
+    if (!cache.promise) {
+        cache.promise = mongoose.connect(MONGO_URI, {
+            bufferCommands: false,
+        });
+    }
+
+    try {
+        cache.conn = await cache.promise;
+    } catch (error) {
+        cache.promise = null;
+        throw error;
+    }
+
+    console.log(`Connected to MongoDB in ${process.env.NODE_ENV} mode`);
+};
